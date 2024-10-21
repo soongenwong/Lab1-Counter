2dda9b9941ac663bf404efb8a28a90eb1e07b1ae
Soong En Wong <wsoongen2015@gmail.com>
2024-10-21 23:48:00 +0100

    completed task2

--- task2/counter_tb.cpp
+++ task2/counter_tb.cpp
@@ -1,6 +1,7 @@
 #include "Vcounter.h"
 #include "verilated.h"
 #include "verilated_vcd_c.h"
+#include "vbuddy.cpp"
 
 int main(int argc, char **argv, char **env)
 {
@@ -17,6 +18,10 @@ int main(int argc, char **argv, char **env)
     top->trace(tfp, 99);
     tfp->open("counter.vcd");
 
+    // initialize vbuddy
+    if (vbdOpen()!=1) return(-1);
+    vbdHeader("Lab 1: Counter");
+
     // initialize simulation inputs
     top->clk = 1;
     top->rst = 1;
@@ -24,7 +29,7 @@ int main(int argc, char **argv, char **env)
     top->count = 0;
 
     // run simulation for many clock cycles
-    for (i = 0; i < 300; i++){
+    for (i = 0; i < 600; i++){
         if(top->count == 9){
             if(a<3){
                 top->en = 0;
@@ -42,13 +47,30 @@ int main(int argc, char **argv, char **env)
             top->eval();
         }
 
+        vbdPlot(int(top->count), 0, 255); 
+        vbdCycle(i+1);
+
         top->rst = (i < 2) | (i == 15);
-        top->en = (i > 4);
+        top->en = vbdFlag();
 
         if (Verilated::gotFinish())
             exit(0);
     }
 
+    // ++++ Send count value to Vbuddy
+    vbdHex(4, (int(top->count) >> 16) & 0xF);
+    vbdHex(3, (int(top->count) >> 8) & 0xF);
+    vbdHex(2, (int(top->count) >> 4) & 0xF);
+    vbdHex(1, int(top->count) & 0xF);
+    vbdCycle(i+1);
+    // ---- end of Vbuddy output section
+
+    // change input stimuli
+    top->rst = (i < 2) | (i == 15);
+    top->en = vbdFlag();
+    if (Verilated::gotFinish()) exit(0);
+
+    vbdClose();  // ++++
     tfp->close();
     exit(0);
 }
